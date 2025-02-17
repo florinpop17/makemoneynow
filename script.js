d589e4d1ef7a3d35ffe50962dcb35fb8e5f44303
Florin Pop <popflorin1705@yahoo.com>
2025-02-17 15:15:28 +0200

    time update + text fix

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Configuration
 const CONFIG = {
-    startDate: new Date("2025-02-17T11:23:00.000+00:00"),
+    startDate: new Date("2025-02-17T15:14:00.000+02:00"),
     daysToSell: 100,
     priceDecreasePerSecond: 0.01,
 };
