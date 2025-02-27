84f841a0c0385d6dd382db7d8b8efe829c2d19ce
Florin Pop <popflorin1705@yahoo.com>
2025-02-27 20:38:25 +0200

    whoops. no ts

--- supabase/functions/send-price-alerts/index.ts
+++ supabase/functions/send-price-alerts/index.ts
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 import { createClient } from "https://esm.sh/@supabase/supabase-js";
 import { Resend } from "https://esm.sh/resend";
